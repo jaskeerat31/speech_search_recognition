a393fd3e53b7ff93fe416f0536dd390326b26751
jaskeerat31 <31588561+jaskeerat31@users.noreply.github.com>
2019-08-14 16:51:26 +0530

    Update speech_assistant.py

--- speech_assistant.py
+++ speech_assistant.py
@@ -1,25 +1,24 @@
    
-
+#importing speech_recognition module
 import speech_recognition as sr
 
+#importing webbrowser module which allows displaying web based documents to users
 import webbrowser
 
 
-
+#setting up the microphone
 sr.Microphone(device_index=1)
 
 
-
+#recognises speech input from microphone
 r=sr.Recognizer()
 
 
-
-
-
+#setting ambient noise levels
 r.energy_threshold=5000
 
 
-
+#use default microphone as audio source for listening
 with sr.Microphone() as source:
 
     print("Speak!")
@@ -28,15 +27,15 @@ with sr.Microphone() as source:
 
     try:
 
-        text=r.recognize_google(audio)
+        text=r.recognize_google(audio) #using google API to recognise audio
 
         print("You said : {}".format(text))
 
-        url='https://www.google.co.in/search?q='
+        url='https://www.google.co.in/search?q=' #the default search url
 
         search_url=url+text
 
-        webbrowser.open(search_url)
+        webbrowser.open(search_url) #opens the required url in the webbrowser
 
     except:
 
